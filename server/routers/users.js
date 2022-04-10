00eed3fe763f91caf48330aa7e2bc581ca177320
Jakub Wiśniewski <kubaczakpl@gmail.com>
2022-04-10 15:17:01 +0200

    creating

---
+++ server/routers/users.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const bcrypt = require('bcrypt');
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+    User.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    let pass = req.body.password
+    let salt = bcrypt.genSaltSync(10);
+    let password = bcrypt.hashSync(pass, salt)
+    const newUser = new User({ username, password });
+
+    newUser.save()
+        .then(() => res.json('User added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
